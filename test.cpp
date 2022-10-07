95ffe461b89426def3f81fa1e49b7640c7ef285a
Vahe <panchoyanvahe92@list.ru>
2022-10-07 19:42:27 +0400

    test

--- test.cpp
+++ test.cpp
@@ -9,20 +9,10 @@ int main() {
 	g.insert_vertex('c');
 	g.insert_vertex('d');
 	g.insert_vertex('h');
-	std::vector<vertex<char>> vertices = g.get_vertex_set();
-	std::cout << "Vertices: {";
-	for (auto it = vertices.begin(); it != vertices.end(); ++it) {
-		std::cout << (*it).value << ' ';
-	}
-	std::cout << "}" << std::endl;
 	g.insert_edge(1, 'a', 'b');
 	g.insert_edge(1, 'b', 'c');
 	g.insert_edge(1, 'd', 'a');
-	std::vector<edge<char>> edges = g.get_edge_set();
-	std::cout << "Edges: {";
-	for (auto it = edges.begin(); it != edges.end(); ++it) {
-		std::cout << "(" << (*it).left << "," << (*it).right << "),";
-	}
-	std::cout << "}" << std::endl;
+	//g.erase_edge('a', 'b');
+	//g.print_graph();
 	return 0;
 }
