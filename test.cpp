54bbbfa0d2893cc2cd504cd4c83d4eb3709f9997
Vahe <panchoyanvahe92@list.ru>
2022-10-09 14:29:26 +0400

    Test

--- test.cpp
+++ test.cpp
@@ -1,19 +1,26 @@
-#include "graph.cpp"
-#include <vector>
+#include "graph.h"
 #include <iostream>
 
 int main() {
-	graph<char> g;
-	g.insert_vertex('a');
-	g.insert_vertex('b');
-	g.insert_vertex('c');
-	g.insert_vertex('d');
-	g.insert_vertex('h');
-	g.insert_edge(1, 'a', 'b');
-	g.insert_edge(1, 'b', 'c');
-	g.insert_edge(1, 'd', 'a');
-	g.erase_edge('a', 'b');
-	g.erase_vertex('c');
-	g.print_graph();
+	graph g;
+	g.insert_vertex(1);
+	g.insert_vertex(2);
+	g.insert_vertex(3);
+	g.insert_vertex(4);
+	g.insert_vertex(5);
+	g.insert_edge(1, 2, 3);
+	g.insert_edge(1, 4, 5);
+	g.insert_edge(2, 4, 3);
+	g.insert_edge(3, 2, 12);
+	g.insert_edge(3, 5, 9);
+	g.insert_edge(4, 5, 6);
+	g.erase_vertex(2);
+	std::cout << g << std::endl;
+	
+	/*std::vector<int> verts = g.get_vertex_set();
+	for (int i = 0; i < verts.size(); ++i) {
+		std::cout << verts[i];
+	}
+	std::cout << std::endl;*/
 	return 0;
 }
