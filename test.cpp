9d14aad1e373d3f2372c484a39ad9b8b4511179e
Vahe <panchoyanvahe92@list.ru>
2022-10-10 12:12:10 +0400

    Test

--- test.cpp
+++ test.cpp
@@ -16,20 +16,24 @@ int main() {
 	g.insert_edge(4, 5, 6);
 	std::cout << g << std::endl;
 	
-	//g.BFS(4);
-	/*graph t;
+	g.BFS(1);
+	g.DFS(5);
+	graph t;
 	t.insert_vertex(1);
 	t.insert_vertex(2);
 	t.insert_vertex(3);
 	t.insert_vertex(4);
 	t.insert_vertex(5);
+	t.insert_vertex(6);
 	t.insert_edge(1, 2, 3);
-	t.insert_edge(1, 4, 5);
-	t.insert_edge(2, 4, 3);
-	t.insert_edge(3, 2, 12);
-	t.insert_edge(3, 5, 9);
-	t.insert_edge(4, 5, 6);
-	std::cout << t << std::endl;*/
+	t.insert_edge(2, 3, 3);
+	t.insert_edge(3, 5, 3);
+	t.insert_edge(5, 4 ,3);
+	t.insert_edge(4 ,2, 3);
+	t.insert_edge(5 ,6, 3);
+	t.BFS(1);
+	t.DFS(1);
+	std::cout << t << std::endl;
 
 	/*std::vector<int> verts = g.get_vertex_set();
 	for (int i = 0; i < verts.size(); ++i) {
