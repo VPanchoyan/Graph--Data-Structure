460e5cfaa0cad47e31e0b4d88bfd9eedf4455214
Vahe <panchoyanvahe92@list.ru>
2022-10-11 23:06:06 +0400

    graph.h

--- graph.h
+++ graph.h
@@ -1,38 +1,43 @@
 #pragma once
 #include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <list>
+#include <initializer_list>
 
 
-struct adjacent_vertex {
-	adjacent_vertex() : weight(0),value(NULL),connect(nullptr) {}
-	int weight;
-	int value;
-	adjacent_vertex* connect;
+template<class T>
+struct Edge{
+	Edge(T v1,T v2,int w) : _vertex1(v1), _vertex2(v2), _weight(w) {}
+	T _vertex1;
+	T _vertex2;
+	int _weight;
 };
 
-struct vertex {
-	vertex(int val) : value(val),connections_head(nullptr) {}
-	int value;
-	adjacent_vertex* connections_head;
-};
-
-class graph {
-	friend std::ostream& operator<<(std::ostream& os, const graph&);
+template<class T>
+class Graph {
+	friend std::ostream& operator<<(std::ostream& os, const Graph<T>&);
 private:
-	std::vector<vertex> vertices;
+	int _size;
+	std::unordered_map<T,std::list<Edge<T>>> _vertices;
 public:
-	graph() {};
-	graph(const graph&) {}; // copy operator assingment
-	graph& operator=(const graph&); // move operator assignment
-	void insert_vertex(int);
-	void insert_edge(int, int,int);
-	void erase_vertex(int);
-	void erase_edge(int, int);
-	std::vector<int> get_vertex_set();
-	std::vector<std::vector<int>> get_edge_set();
-	bool operator==(const graph&);
-	bool operator!=(const graph&);
-	void BFS(int);
-	void DFS(int);
-};
+	Graph() : _size(0) {};
+	//Graph(std::initializer_list<std::pair<T, std::initializer_list<T>>> list);
+	//graph(const graph& graph_to_copy) {	this->vertices = graph_to_copy.vertices;}; // copy operator assingment
+	//graph& operator=(const graph&); // move operator assignment
+	void insert_vertex(T value);
+	void insert_edge(T vertex1,T vertex2,int weight);
+	void erase_vertex(T vertex);
+	void erase_edge(T vertex1, T vertex2);
+	std::vector<T> get_vertex_set();
+	std::vector<std::tuple<T,T,T>> get_edge_set();
+	bool operator==(const Graph&);
+	bool operator!=(const Graph&);
+	int get_number_of_vertex();
+	void BFS(T start_vertex);
+	//void DFS(Tstart_vertex);
+	//void find_path(T vertex1, T vertex2);
 
+	bool vertex_exists(T vertex);
+	void print_edges();
+	void print_vertices();
+};
