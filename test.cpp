2acecd7f2513e01dd79c28feed6e8f963c8e9646
Vahe <panchoyanvahe92@list.ru>
2022-10-11 23:06:28 +0400

    Test

--- test.cpp
+++ test.cpp
@@ -1,8 +1,8 @@
-#include "graph.h"
+#include "graph.hpp"
 #include <iostream>
 
 int main() {
-	graph g;
+	Graph<int> g;
 	g.insert_vertex(1);
 	g.insert_vertex(2);
 	g.insert_vertex(3);
@@ -12,33 +12,24 @@ int main() {
 	g.insert_edge(1, 4, 5);
 	g.insert_edge(2, 4, 3);
 	g.insert_edge(3, 2, 12);
-	g.insert_edge(3, 5, 9);
 	g.insert_edge(4, 5, 6);
-	std::cout << g << std::endl;
-	
-	g.BFS(1);
-	g.DFS(5);
-	graph t;
+	g.insert_edge(3, 5, 9);
+
+	Graph<int> t;
 	t.insert_vertex(1);
 	t.insert_vertex(2);
 	t.insert_vertex(3);
 	t.insert_vertex(4);
 	t.insert_vertex(5);
-	t.insert_vertex(6);
 	t.insert_edge(1, 2, 3);
-	t.insert_edge(2, 3, 3);
-	t.insert_edge(3, 5, 3);
-	t.insert_edge(5, 4 ,3);
-	t.insert_edge(4 ,2, 3);
-	t.insert_edge(5 ,6, 3);
-	t.BFS(1);
-	t.DFS(1);
-	std::cout << t << std::endl;
-
-	/*std::vector<int> verts = g.get_vertex_set();
-	for (int i = 0; i < verts.size(); ++i) {
-		std::cout << verts[i];
-	}
-	std::cout << std::endl;*/
+	t.insert_edge(1, 4, 5);
+	t.insert_edge(2, 4, 3);
+	t.insert_edge(3, 2, 12);
+	t.insert_edge(4, 5, 6);
+	t.insert_edge(3, 5, 9);
+	//std::cout << (g == t) << "\n";
+	/*g.print_vertices();
+	g.print_edges();*/
+	std::cout << t;
 	return 0;
 }
