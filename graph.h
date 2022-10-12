4139219be3b7724a92065a80615e89b46d8a58c6
Vahe <panchoyanvahe92@list.ru>
2022-10-12 16:54:13 +0400

    Graph.h

--- graph.h
+++ graph.h
@@ -13,13 +13,28 @@ class Graph {
 		S _vertex2;
 		int _weight;
 	};
+	template<class T>
 	friend std::ostream& operator<<(std::ostream& os, const Graph<T>&);
 private:
 	int _size;
 	std::unordered_map<T,std::list<Edge<T>>> _vertices;
 public:
 	Graph() : _size(0) {};
-	//Graph(std::initializer_list<std::pair<T, std::initializer_list<T>>> list);
+	/*Graph(const Graph<T>&& new_graph) : _size(0), _vertices(NULL) {
+		_vertices = new_graph._vertices;
+		_size = new_graph._size;
+		new_graph._size = 0;
+		new_graph._size = NULL;
+	}
+	Graph<T>& operator=(const Graph<T>&& new_graph) {
+		if (this != new_graph) {
+			_vertices = new_graph._vertices;
+			_size = new_graph._size;
+			new_graph._size = 0;
+			new_graph._size = NULL;
+		}
+		return *this;
+	}*/
 	Graph<T> operator=(const Graph<T>&);
 	void insert_vertex(T value);
 	void insert_edge(T vertex1,T vertex2,int weight);
@@ -27,13 +42,13 @@ public:
 	void erase_edge(T vertex1, T vertex2);
 	std::vector<T> get_vertex_set();
 	std::vector<std::tuple<T,T,T>> get_edge_set();
-	//bool operator==(const Graph&);
+	bool operator==(const Graph&);
 	//bool operator!=(const Graph&);
 	int get_number_of_vertex();
 	void BFS(T start_vertex);
 	void DFS(T start_vertex);
-	//void find_path(T vertex1, T vertex2);
+	void find_path(T vertex1, T vertex2);
 	bool vertex_exists(T vertex);
-	void print_edges();
-	void print_vertices();
+	void print_edges() ;
+	void print_vertices() ;
 };
