c9a96fc52ea887fa7907b9f672108613b991c39d
Vahe <panchoyanvahe92@list.ru>
2022-10-12 10:53:21 +0400

    graph.h

--- graph.h
+++ graph.h
@@ -5,16 +5,18 @@
 #include <initializer_list>
 
 
-template<class T>
-struct Edge{
-	Edge(T v1,T v2,int w) : _vertex1(v1), _vertex2(v2), _weight(w) {}
-	T _vertex1;
-	T _vertex2;
-	int _weight;
-};
+
+
 
 template<class T>
 class Graph {
+	template<class S>
+	struct Edge {
+		Edge(S v1, S v2, int w) : _vertex1(v1), _vertex2(v2), _weight(w) {}
+		S _vertex1;
+		S _vertex2;
+		int _weight;
+	};
 	friend std::ostream& operator<<(std::ostream& os, const Graph<T>&);
 private:
 	int _size;
@@ -29,8 +31,8 @@ public:
 	void erase_edge(T vertex1, T vertex2);
 	std::vector<T> get_vertex_set();
 	std::vector<std::tuple<T,T,T>> get_edge_set();
-	bool operator==(const Graph&);
-	bool operator!=(const Graph&);
+	//bool operator==(const Graph&);
+	//bool operator!=(const Graph&);
 	int get_number_of_vertex();
 	void BFS(T start_vertex);
 	void DFS(T start_vertex);
