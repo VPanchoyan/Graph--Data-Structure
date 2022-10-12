d0603ed0854854adff98c290c10cfe0701d9b6e6
Vahe <panchoyanvahe92@list.ru>
2022-10-12 10:14:26 +0400

    Test

--- test.cpp
+++ test.cpp
@@ -8,28 +8,16 @@ int main() {
 	g.insert_vertex(3);
 	g.insert_vertex(4);
 	g.insert_vertex(5);
-	g.insert_edge(1, 2, 3);
-	g.insert_edge(1, 4, 5);
-	g.insert_edge(2, 4, 3);
-	g.insert_edge(3, 2, 12);
-	g.insert_edge(4, 5, 6);
-	g.insert_edge(3, 5, 9);
-
-	Graph<int> t;
-	t.insert_vertex(1);
-	t.insert_vertex(2);
-	t.insert_vertex(3);
-	t.insert_vertex(4);
-	t.insert_vertex(5);
-	t.insert_edge(1, 2, 3);
-	t.insert_edge(1, 4, 5);
-	t.insert_edge(2, 4, 3);
-	t.insert_edge(3, 2, 12);
-	t.insert_edge(4, 5, 6);
-	t.insert_edge(3, 5, 9);
-	//std::cout << (g == t) << "\n";
-	/*g.print_vertices();
-	g.print_edges();*/
-	std::cout << t;
+	g.insert_edge(1, 4, 1);
+	g.insert_edge(2, 4, 1);
+	g.insert_edge(1, 2, 1);
+	g.insert_edge(3, 2, 1);
+	g.insert_edge(4, 5, 1);
+	g.insert_edge(3, 5, 1);
+	g.print_vertices();
+	g.print_edges();
+	//std::cout << t;
+	g.BFS(2);
+	g.DFS(1);
 	return 0;
 }
